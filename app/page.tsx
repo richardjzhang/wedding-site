a0c0195ac9499e15c188384db5892728dae6ed38
richardjzhang <hello@richardjzhang.com>
2024-03-01 18:14:02 +1100

    Use alternative image

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,18 @@ import Image from "@/components/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      <div className="h-screen">
+      <div className="h-screen relative w-screen">
+        {/* <text className="tracking-widest text-sm absolute left-[45%] top-1/3 z-10">
+          WEDDING OF
+        </text> */}
         <Image
           layout="fill"
           objectFit="contain"
-          src="/flowers-hero-transparent.png"
+          src="/flowers-hero-2-alt.webp"
           alt="hero"
         />
       </div>
-      <div className="p-24">hello</div>
+      <div className="p-24 ">WEDDING OF</div>
     </main>
   );
 }

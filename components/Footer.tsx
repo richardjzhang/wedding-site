e097675eb6c42f4c19494e7cad48f2f643f09d28
richardjzhang <hello@richardjzhang.com>
2024-04-19 12:58:42 +1000

    Convert to app router

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import Home from '../../public/assets/icons/home.svg';
-import Calender from '../../public/assets/icons/calender.svg';
-import Gallery from '../../public/assets/icons/gallery.svg';
-import Chat from '../../public/assets/icons/chat.svg';
+import Image from 'next/image';
 
 const Footer = () => {
   return (
@@ -10,19 +7,40 @@ const Footer = () => {
       <div className="bg-dark-500 w-full px-6 rounded-full shadow border-opacity-20 border border-white">
         <ul className=" flex items-center justify-evenly text-white text-xs font-light py-2 md:py-3">
           <li className="flex items-center font-medium  flex-col px-2 py-1">
-            <Home className="h-6 w-6 mb-1" />
+            <Image
+              src="/assets/icons/home.svg"
+              alt="home"
+              width={24}
+              height={24}
+            />
+
             <p>Beranda</p>
           </li>
           <li className="flex items-center  flex-col px-2 py-1">
-            <Calender className="h-6 w-6 mb-1" />
+            <Image
+              src="/assets/icons/calender.svg"
+              alt="calender"
+              width={24}
+              height={24}
+            />
             <p>Resepsi</p>
           </li>
           <li className="flex items-center  flex-col px-2 py-1">
-            <Gallery className="h-6 w-6 mb-1" />
+            <Image
+              src="/assets/icons/gallery.svg"
+              alt="gallery"
+              width={24}
+              height={24}
+            />
             <p>Galeri</p>
           </li>
           <li className="flex items-center  flex-col px-2 py-1">
-            <Chat className="h-6 w-6 mb-1" />
+            <Image
+              src="/assets/icons/chat.svg"
+              alt="chat"
+              width={24}
+              height={24}
+            />
             <p>Ucapan</p>
           </li>
         </ul>
